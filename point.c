3bdce7963ff4841d242d37e1c7feadea26046d50
Apurva <58025903+apurvagurme@users.noreply.github.com>
2020-04-13 00:18:46 +0530

    Modified get_closest_food function

--- point.c
+++ point.c
@@ -5,7 +5,7 @@
 void get_closest_food(struct Point food_points[], int points_length, struct Point current_location, struct Point *closest_food_location)
 {
   int distance;
-  double previous_distance = -INFINITY;
+  double previous_distance = INFINITY;
 
   for(int index = 0; index < points_length; index++)
   {
